3041716ed41fe168edfff0db793c7258b1a8c24a
Erick Freire <erilefr@gmail.com>
2022-10-29 21:12:59 -0300

    v1
    v1

---
+++ Nome/src/br/com/erickfreire/nome/Nome.java
@@ -0,0 +1,13 @@
+package br.com.erickfreire.nome;
+
+import javax.swing.JOptionPane;
+
+public class Nome {
+
+	public static void main(String[] args) {
+		
+		JOptionPane.showMessageDialog(null, "Nome: Erick Freire");
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # NomeCaixaDeDialogo
  Programa que exibe o nome em uma caixa de diálogo
+
+ ## Imagem
+ ![Caixa de Mensagem com Nome](nome.png)
